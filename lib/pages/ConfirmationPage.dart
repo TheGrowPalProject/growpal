5401fa829b6fdb180ad12cfd4ce034017d1bde90
MonkeyDGod619 <theuniverseisflat101@gmail.com>
2022-10-22 05:30:43 +0530

    Vibration implemented

--- lib/pages/ConfirmationPage.dart
+++ lib/pages/ConfirmationPage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class ConfirmationPage extends StatefulWidget {
   const ConfirmationPage({Key? key}) : super(key: key);
@@ -13,20 +14,20 @@ class _ConfirmationPageState extends State<ConfirmationPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-        children: [SafeArea(
-          child: Padding(
-            padding: EdgeInsets.all(20),
-            child: Text(
-              "Are You Sure?",
-              style: TextStyle(
-                color: Color(0xFF4C53A5),
-                fontSize: 80,
-                fontWeight: FontWeight.bold,
+        children: [
+          SafeArea(
+            child: Padding(
+              padding: EdgeInsets.all(20),
+              child: Text(
+                "Are You Sure?",
+                style: TextStyle(
+                  color: Color(0xFF4C53A5),
+                  fontSize: 80,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ),
           ),
-        ),
-
         ],
       ),
       bottomNavigationBar: BottomAppBar(
@@ -39,24 +40,24 @@ class _ConfirmationPageState extends State<ConfirmationPage> {
               InkWell(
                 onTap: () {
                   Navigator.pushNamed(context, "checkout");
+                  HapticFeedback.heavyImpact();
                 },
                 child: Container(
-                    alignment: Alignment.center,
-                    height: 50,
-                    width: double.infinity,
-                    decoration: BoxDecoration(
-                      color: Color(0xFF4C53A5),
-                      borderRadius: BorderRadius.circular(20),
-
+                  alignment: Alignment.center,
+                  height: 50,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: Color(0xFF4C53A5),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Text(
+                    "YES",
+                    style: TextStyle(
+                      fontSize: 26,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
                     ),
-                    child: Text(
-                      "YES",
-                      style: TextStyle(
-                        fontSize: 26,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                      ),
-                    ),
+                  ),
                 ),
               ),
               InkWell(
@@ -70,7 +71,6 @@ class _ConfirmationPageState extends State<ConfirmationPage> {
                   decoration: BoxDecoration(
                     color: Color(0xFF4C53A5),
                     borderRadius: BorderRadius.circular(20),
-
                   ),
                   child: Text(
                     "NO",
@@ -82,12 +82,10 @@ class _ConfirmationPageState extends State<ConfirmationPage> {
                   ),
                 ),
               ),
-
             ],
           ),
         ),
       ),
     );
-
   }
 }
